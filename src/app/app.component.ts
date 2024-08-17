dfaecfe3a0833c1fb1a57c6a506b562f9ea59abd
cliffordbechtel <crbechtel@gmail.com>
2024-08-17 06:58:45 -0400

    Adding-cmp-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentInput } from "./investment-input.model";
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
-  selector: "app-root",
-  standalone: true,
-  templateUrl: "./app.component.html",
-  imports: [HeaderComponent, UserInputComponent],
+    selector: "app-root",
+    standalone: true,
+    templateUrl: "./app.component.html",
+    imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {
-  onCalculateInvestmentResults(data: {
-    initialInvestment: number;
-    duration: number;
-    expectedReturn: number;
-    annualInvestment: number;
-  }) {
+    resultsData: {
+          year: number,
+          interest: number,
+          valueEndOfYear: number,
+          annualInvestment: number,
+          totalInterest: number,
+          totalAmountInvested: number,
+        }[];
+        
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } =
       data;
     const annualData = [];
